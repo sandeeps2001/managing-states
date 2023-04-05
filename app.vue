a6104ac06bc00f75c10061c8c4e583d912d6c7e8
Sandy <sandy4adhi@gmail.com>
2023-04-05 19:03:36 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,30 @@
+
+import Cards from './components/Cards.vue';
+
+
+import Heading from './components/Heading.vue';
+
+
+import Nav from './components/Nav.vue';
+:style="isDarkMode ?{backgroundColor: 'black'} : null"
+:style ="isDarkMode ?{backgroundColor: 'black'} : null"
+
+
+<script setup lang="ts">
+const {isDarkMode,toggleDarkMode} = useDarkMode();
+</script>
+<template> 
+  <div :style="isDarkMode ? {backgroundColor:'black'}:null"> 
+    <Nav />
+    <Heading/>
+    <Cards/>
+  </div>
+</template>
+
+<style >
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Georgia;
+}
+</style>
---
+++ composables/useDarkMode.ts
@@ -0,0 +1,15 @@
+const useDarkMode = () =>{
+    const isDarkMode = useState("darkMode", () => false);
+
+   
+const toggleDarkMode = () =>{
+    isDarkMode.value = !isDarkMode.value
+
+}
+return{
+    isDarkMode,
+    toggleDarkMode
+};
+};
+
+export default useDarkMode;
